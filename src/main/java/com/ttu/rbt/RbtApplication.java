8bb043e21693efd75527f2c3cd62e1aa1279c44c
GopiChand reddy <gopichandureddy@GopiChands-MacBook-Pro.local>
2021-05-10 11:40:08 -0500

    uodated

--- src/main/java/com/ttu/rbt/RbtApplication.java
+++ src/main/java/com/ttu/rbt/RbtApplication.java
@@ -1,5 +1,15 @@
 package com.ttu.rbt;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +18,41 @@ public class RbtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RbtApplication.class, args);
+		
+		final String username = "gdoggala@ttu.edu";  // like yourname@outlook.com
+	    final String password = "sRINUVASU1901@";   // password here
+
+	    Properties props = new Properties();
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.smtp.host", "smtp-mail.outlook.com");
+	    props.put("mail.smtp.port", "587");
+
+	    Session session = Session.getInstance(props,
+	      new javax.mail.Authenticator() {
+	        @Override
+	        protected PasswordAuthentication getPasswordAuthentication() {
+	            return new PasswordAuthentication(username, password);
+	        }
+	      });
+	    session.setDebug(true);
+
+	    try {
+
+	        Message message = new MimeMessage(session);
+	        message.setFrom(new InternetAddress(username));
+	        message.setRecipients(Message.RecipientType.TO,
+	            InternetAddress.parse("rmadishe@ttu.edu"));   // like inzi769@gmail.com
+	        message.setSubject("Test");
+	        message.setText("HI you have done sending mail with outlook");
+
+	        Transport.send(message);
+
+	        System.out.println("Done");
+
+	    } catch (MessagingException e) {
+	        throw new RuntimeException(e);
+	    }
 		
 	}
 	
